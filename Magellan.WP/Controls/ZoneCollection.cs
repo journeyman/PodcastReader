c675b87723257a4b6d72fb4797160fb7ad820804
Stas Shusha <journeymanmw@gmail.com>
2013-03-22 19:09:42 +0300

    added Magellan (layout engine for XAML), implemented PodcastView with it (yet unseccessfully)

---
+++ Magellan.WP/Controls/ZoneCollection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Magellan.WP.Controls
+{
+    /// <summary>
+    /// Represents a collection of <see cref="Zone">zones</see>.
+    /// </summary>
+    public class ZoneCollection : ObservableCollection<Zone>
+    {
+    }
+}
