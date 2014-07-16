00890cbcfe915f6623c379f654ac802f0d98554e
Stas Shusha <journeymanmw@gmail.com>
2014-07-16 20:22:31 +0300

    made FeedPreviewsLoader lazily update itself on subscribe

--- src/Clients/Phone8/PodcastReader.Phone8/Loaders/FeedPreviewsLoader.cs
+++ src/Clients/Phone8/PodcastReader.Phone8/Loaders/FeedPreviewsLoader.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Net.Http;
 using System.Reactive.Linq;
-using System.Reactive.Subjects;
 using System.Reactive.Threading.Tasks;
 using PodcastReader.Infrastructure.Interfaces;
 using PodcastReader.Infrastructure.Utils;
@@ -21,38 +20,46 @@ namespace PodcastReader.Phone8.Loaders
     public class FeedPreviewsLoader : IFeedPreviewsLoader, IEnableLogger
     {
         private readonly ISubscriptionsManager _subscriptionsManager;
+        private readonly IObservable<IFeedPreview> _feedsObservable;
 
         public FeedPreviewsLoader(ISubscriptionsManager subscriptionsManager)
         {
             _subscriptionsManager = subscriptionsManager;
+
+            var client = new HttpClient();
+
+            var predefinedFeeds = new[]
+                                  {
+                                      new Uri(TEST_FEED_URL0),
+                                      new Uri(TEST_FEED_URL1),
+                                      new Uri(TEST_FEED_URL2),
+                                      new Uri(TEST_FEED_URL3),
+                                      new Uri(TEST_FEED_URL4),
+                                  };
+            
+            _feedsObservable = _subscriptionsManager.Subscriptions
+                    .Select(s => s.Uri)
+                    .StartWith(predefinedFeeds)
+                    .Distinct()
+                    .SelectMany(uri => client.GetStringAsync(uri).ToObservable())
+                    .Select(FeedXmlParser.Parse)
+                    .Select(feed => new FeedViewModel(feed.Title.Text, new PodcastItemsLoader(feed)))
+                    .LoggedCatch<IFeedPreview, FeedPreviewsLoader, Exception>(this, ex => Observable.Throw<IFeedPreview>(new FeedLoadingException(ex)));
         }
 
-        private const string TEST_FEED_URL = "http://feeds.feedburner.com/Hanselminutes?format=xml";
+        private const string TEST_FEED_URL0 = "http://feeds.feedburner.com/Hanselminutes?format=xml";
         private const string TEST_FEED_URL1 = "http://feeds.feedburner.com/netRocksFullMp3Downloads?format=xml";
         private const string TEST_FEED_URL2 = "http://hobbytalks.org/rss.xml";
         private const string TEST_FEED_URL3 = "http://haskellcast.com/feed.xml";
         private const string TEST_FEED_URL4 = "http://thespaceshow.wordpress.com/feed/";
 
-        readonly ISubject<IFeedPreview> _subject = new ReplaySubject<IFeedPreview>();
-
         public IDisposable Subscribe(IObserver<IFeedPreview> observer)
         {
-            return _subject.Subscribe(observer);
+            return _feedsObservable.Subscribe(observer);
         }
 
         public async void Load()
         {
-            var client = new HttpClient();
-
-            _subscriptionsManager.Subscriptions
-                .Select(s => s.Uri)
-                .StartWith(new Uri(TEST_FEED_URL), new Uri(TEST_FEED_URL1), new Uri(TEST_FEED_URL2))
-                .SelectMany(uri => client.GetStringAsync(uri).ToObservable())
-                .Select(FeedXmlParser.Parse)
-                .Select(feed => new FeedViewModel(feed.Title.Text, new PodcastItemsLoader(feed)))
-                .LoggedCatch<IFeedPreview, FeedPreviewsLoader, Exception>(this, ex => Observable.Throw<IFeedPreview>(new FeedLoadingException(ex)))
-                .Subscribe(_subject);
-
             await _subscriptionsManager.ReloadSubscriptions();
         }
     }
