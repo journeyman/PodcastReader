c5c2423e2c658983439328484dd69836a5dde435
journeyman <journeymanmw@gmail.com>
2015-02-08 17:11:50 +0000

    few design tweaks

--- src/Clients/Phone8/PodcastReader.Phone8/Views/Design/DesignFeedViewModel.cs
+++ src/Clients/Phone8/PodcastReader.Phone8/Views/Design/DesignFeedViewModel.cs
@@ -10,9 +10,31 @@ namespace PodcastReader.Phone8.Views.Design
 {
     public class DesignFeedViewModel : IFeed, IFeedPreview
     {
+        public DesignFeedViewModel()
+        {
+            var date = DateTimeOffset.Now;
+            var summary = "fsjfajfj ajf]ja se]fja]sjfsjf] afas fi f a]fj asfp ashfuh as[fh [as faf e shafsef hasvnpisna a";
+            var title = "podcast item title";
+            var uri = new Uri("http://google.com");
+
+            Items = new []
+                {
+                    new DesignFeedItem { DatePublished = date, Title = title, Summary = summary, PodcastUri = uri},
+                    new DesignFeedItem { DatePublished = date, Title = title, Summary = summary, PodcastUri = uri},
+                    new DesignFeedItem { DatePublished = date, Title = title, Summary = summary, PodcastUri = uri},
+                    new DesignFeedItem { DatePublished = date, Title = title, Summary = summary, PodcastUri = uri},
+                    new DesignFeedItem { DatePublished = date, Title = title, Summary = summary, PodcastUri = uri},
+                    new DesignFeedItem { DatePublished = date, Title = title, Summary = summary, PodcastUri = uri},
+                    new DesignFeedItem { DatePublished = date, Title = title, Summary = summary, PodcastUri = uri},
+                    new DesignFeedItem { DatePublished = date, Title = title, Summary = summary, PodcastUri = uri},
+                    new DesignFeedItem { DatePublished = date, Title = title, Summary = summary, PodcastUri = uri},
+                };
+        }
+
         public string Title { get; set; }
         public IFeedItem LastFeedItem { get;set; }
         public DateTimeOffset LatestPublished { get; set; }
+        public IEnumerable<IPodcastItem> Items { get; set; } 
     }
 
     public class DesignFeedItem : IPodcastItem
